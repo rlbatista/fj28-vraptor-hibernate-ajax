c578d094e74b1c586783b1d2006c33dd082b90df
Renato <r.lopes@desktop.com.br>
2013-07-30 11:53:17 -0300

    Exercicio 9.6

--- src/br/com/caelum/godbuy/infra/CriadorDeSessaoFactory.java
+++ src/br/com/caelum/godbuy/infra/CriadorDeSessaoFactory.java
@@ -1,20 +1,34 @@
 package br.com.caelum.godbuy.infra;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.ComponentFactory;
 
 @SuppressWarnings("deprecation")
-@Component
+@Component @ApplicationScoped
 public class CriadorDeSessaoFactory implements ComponentFactory<SessionFactory> {
 
+	private SessionFactory factory;
+
 	public SessionFactory getInstance() {
+		return this.factory;
+	}
+	
+	@PostConstruct
+	public void abre() {
 		AnnotationConfiguration cfg = new AnnotationConfiguration();
 		cfg.configure();
-		SessionFactory factory = cfg.buildSessionFactory();
-		return factory;
+		this.factory = cfg.buildSessionFactory();		
 	}
-
+	
+	@PreDestroy
+	public void fecha() {
+		this.factory.close();
+	}
 }
--- src/br/com/caelum/godbuy/infra/CriadorDeSession.java
+++ src/br/com/caelum/godbuy/infra/CriadorDeSession.java
@@ -1,5 +1,8 @@
 package br.com.caelum.godbuy.infra;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 
@@ -9,14 +12,23 @@ import br.com.caelum.vraptor.ioc.ComponentFactory;
 @Component
 public class CriadorDeSession implements ComponentFactory<Session> {
 	private final SessionFactory factory;
+	private Session session;
 	
 	public CriadorDeSession(SessionFactory factory) {
 		this.factory = factory;
 	}
 	
 	public Session getInstance() {
-		Session session = factory.openSession();
-		return session;
+		return this.session;
 	}
 
+	@PostConstruct
+	public void abre() {
+		this.session = factory.openSession();		
+	}
+	
+	@PreDestroy
+	public void fecha() {
+		this.session.close();
+	}
 }
